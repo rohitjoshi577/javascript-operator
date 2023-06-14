173cb7ec2bb617bac2077352d1c817eae49a6f29
rohit joshi <developerjoshi577@gmail.com>
2023-06-14 08:05:08 +0530

    solved basic js operator based questions

---
+++ maxOfBoth.js
@@ -0,0 +1,9 @@
+function max(a,b){
+    if(a>b){console.log(` ${a} is bigger`)}
+    else if(b>a){console.log(` ${b} is bigger`)}
+    else{console.log("both are equal")}
+}
+
+max(1,2);
+max(2,1);
+max(2,2);
---
+++ operatorbased.js
@@ -0,0 +1,9 @@
+// operator based
+
+// sum 
+
+function sum(a,b){
+    console.log(a+b);
+}
+
+sum(3,4);
